21b4dcb9cd4678255bf18d9dacab23cd033a42dc
Sanalog <111274688+itsSanalog@users.noreply.github.com>
2023-09-08 06:28:21 +0800

    fsil

--- svelog/src/routes/index.svelte
+++ svelog/src/routes/index.svelte
@@ -1,9 +1,30 @@
-<script>
-    //JS
-</script>
-
+<!-- /src/routes/__layout.svelte -->
 <style>
-    /* css */
+    header {
+        display: flex;
+        justify-content: space-between;
+        border-bottom: 1px solid lightgray;
+        padding-left: 4vw;
+        padding-right: 4vw;
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    h1 {
+        margin: 5;
+        font-weight: 400;
+        font-size: 25px;
+    }
 </style>
 
-<!-- HTML -->
+<header>
+    <h1>My Blog</h1>
+    <nav>
+        <a href="/">Home</a>
+    </nav>
+</header>
+<slot></slot>
