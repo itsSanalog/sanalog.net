9e040f39c786062053cf8110293d0cfeb183b1d2
Sanalog <111274688+itsSanalog@users.noreply.github.com>
2023-11-02 23:10:01 +0800

    combacktime

--- js/sanalog.js
+++ js/sanalog.js
@@ -2,6 +2,7 @@
 const button = document.getElementById("burger");
 button.addEventListener("click", () => {
     // To make the button work on first click, navmenu is set to -380px in inline html. Thus the sequence is 0?1:0. The rest, the default is set to whatever value fits 0 state in the css file. Thus, sequence is 1?0:1
+    // It's been 20 minutes since i wrote that description and it doesn't make sense anymore, how is me in 3 years supposed to understand this
     document.getElementById("navmenu").style.transform =
         (document.getElementById("navmenu").style.transform == "translateX(-380px)") ? "translateX(0)" : "translateX(-380px)";
     document.getElementsByClassName("yellow")[0].style.opacity =
