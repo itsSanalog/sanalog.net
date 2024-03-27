a46d08fb70ad613e4e35ed20876d9d38303e57fd
Sanalog <111274688+itsSanalog@users.noreply.github.com>
2024-03-28 01:05:40 +0800

    fixing

--- sanastro/src/scripts/navmenu.js
+++ sanastro/src/scripts/navmenu.js
@@ -3,6 +3,9 @@ const button = document.getElementById("burger");
 button.addEventListener("click", () => {
     // To make the button work on first click, navmenu is set to -380px in inline html. Thus the sequence is 0?1:0. The rest, the default is set to whatever value fits 0 state in the css file. Thus, sequence is 1?0:1
     // It's been 20 minutes since i wrote that description and it doesn't make sense anymore, how is me in 3 years supposed to understand this
+
+    // Hi, me in three years here. i do not understand this.
+    
     document.getElementById("navmenu").style.transform =
         (document.getElementById("navmenu").style.transform == "translateX(-380px)") ? "translateX(0)" : "translateX(-380px)";
     document.getElementsByClassName("yellow")[0].style.opacity =
