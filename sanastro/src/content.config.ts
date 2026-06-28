a26f75c703ad30fe42f2742d24a1fed6bca0a57d
Sanalog <topgeon@gmail.com>
2026-06-28 13:16:59 +0800

    [big] Move to Astro v6 six days after v7 comes out (67 haha)

--- sanastro/src/content.config.ts
+++ sanastro/src/content.config.ts
@@ -1,29 +1,31 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod"
+import { glob } from "astro/loaders";
 
 // 2. Define a `type` and `schema` for each collection
 const blogCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/blog" }),
   schema: ({ image }) => z.object({  // not using ({ image }) will break stuff
     cover: image().optional(),
     coverAlt: z.string().optional(),
     banner: image().optional(),
     bannerAlt: z.string().optional(),
-
+    
     title: z.string(),  // required
     description: z.string().optional(),
     author: z.string().optional().default('Sanalog'),
     creationDate: z.string().optional(),
-
+    
     publicationDate: z.date().optional(),
     sortOrder: z.number().optional(),
-
+    
     // "slug" is a special, reserved property name that is not allowed in custom collection schema
   }),
 });
 
 const reviewsCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/reviews" }),
   schema: z.object({
     imageLink: z.string().optional(),
     imageAlt: z.string().optional(),
